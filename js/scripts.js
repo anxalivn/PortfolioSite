55ad07194475e357dd3b5de4f113138aa144aaaf
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2021-12-07 23:43:19 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+var $circle = $('.circle');
+
+function moveCircle(e) {
+	TweenLite.to($circle, 0.3, {
+    css: {
+      left: e.pageX + 45,
+      top: e.pageY + 45
+    }
+  });
+}
+
+$(window).on('mousemove', moveCircle);
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+var square = document.getElementsByClassName('square');
+
+for(var i = 0; i<square.length; i++){
+  square[i].addEventListener('mouseenter', function(){
+    this.classList.add("rubberBand");
+    this.addEventListener("animationend", function(){
+      this.classList.remove("rubberBand");
+    }, false);
+  })
+}
